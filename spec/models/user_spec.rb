f922bf08cc2862b3928573a12fbc8aa90925550d
Eli Block <18074026+eliblocks@users.noreply.github.com>
2024-10-24 16:34:07 -0400

    fix specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,20 +34,6 @@ RSpec.describe User do
     end
   end
 
-  describe "#best_match", :vcr do
-    it "returns the best matching user" do
-      user1 = create(:user, id: 1, status: "searching")
-      user2 = create(:user, id: 2, status: "searching")
-      user3 = create(:user, id: 3, status: "searching")
-
-      user1.messages.create(role: "user", content: "I'm a software engineer in NYC with experience using Ruby on Rails at several SaaS startups. Looking for a new in-person position with good benefits.")
-      user2.messages.create(role: "user", content: "I'm looking for a roommate to sign a new lease in Park Slope, Brooklyn")
-      user3.messages.create(role: "user", content: "I'm a founder at an early stage SaaS startup with a great product looking to bring on experienced engineers available to work in person in NYC")
-
-      expect(user1.best_match).to eq(user3)
-    end
-  end
-
   describe "#search", :vcr do
     it "creates a match" do
       user1 = create(:user, id: 1, status: "searching")
@@ -56,9 +42,11 @@ RSpec.describe User do
 
       user1.messages.create(role: "user", content: "I'm a software engineer in NYC with experience using Ruby on Rails at several SaaS startups. Looking for a new in-person position with good benefits and work life balance.")
       user2.messages.create(role: "user", content: "I'm looking for a roommate to sign a new lease in Park Slope, Brooklyn")
-      user3.messages.create(role: "user", content: "I'm a founder at an early stage SaaS startup with a great product looking to bring on experienced full stack web developers with SaaS experience who are available to work full time in person in NYC.")
+      user3.messages.create(role: "user", content: "I'm a founder at an early stage SaaS startup with a great product looking to bring on full stack web developers with 2-4 years SaaS experience who are available to work full time in person in NYC.")
 
       user1.search
+      user2.search
+      user3.search
 
       expect(user1.reload.matched_user).to eq(user3)
       expect(user1.status).to eq("matched")
