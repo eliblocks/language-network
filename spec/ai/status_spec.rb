939c5345e25fea6831d16f975cb2196aaee170f8
Eli Block <18074026+eliblocks@users.noreply.github.com>
2024-11-26 17:11:40 -0500

    add spec

--- spec/ai/status_spec.rb
+++ spec/ai/status_spec.rb
@@ -48,4 +48,22 @@ RSpec.describe "Status", :vcr, type: :model do
       expect(user.status).to eq("searching")
     end
   end
+
+  it "sets job search back to drafting" do
+    messages = [
+      { role: "user", content: "Hello" },
+      { role: "assistant", content: "Hello! I'm a bot that can connect you to people. Tell me a little about what you're looking for and I'll try to find someone relevant to you." },
+      { role: "user", content: "I'm a software engineer in NYC with experience using Ruby on Rails at several SaaS startups. Looking for a new in-person position with good benefits and work life balance." },
+      { role: "assistant", content: "Great! I'll let you know if I find someone looking for an engineer with your skills." },
+      { role: "assistant", content: "I have found a potential connection for you. Bob is a founder at an early stage SaaS startup in NYC looking to bring on full stack web developers with 2-4 years of SaaS experience for full-time in-person work. This opportunity might align well with your background and career goals. https://www.instagram.com." },
+      { role: "user", content: "Thank you, that sounds like a great fit!" }
+    ]
+    messages.map! { |attrs| Message.new(attrs) }
+    user = create(:user)
+    user.messages << messages
+    10.times do
+      user.update_status
+      expect(user.status).to eq("drafting")
+    end
+  end
 end
